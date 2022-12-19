6c3882b717752445244e771ca6a73a8a1ecb6030
LeviJunior1 <levijjunior55@gmail.com>
2022-12-19 15:36:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const socialMedia = [
+  {
+    title: "Linkedin",
+    link: "https://www.linkedin.com/in/levi-junior-130719130/"
+  },
+  {
+    title: "GitHub",
+    link: "https://github.com/LeviJunior1"
+  },
+  {
+    title: "Youtube",
+    link: "https://www.youtube.com/channel/UCQ2G26Bg2To_jMGmc1q67wg"
+  }
+]
+
+function createLink(media) {
+  const newLink = document.createElement("a")
+  newLink.href = media.link
+  newLink.target = "_blank"
+  newLink.textContent = media.title
+  return newLink
+}
+
+function setLinkDOM(link) {
+  const mainElement = document.querySelector("main")
+  mainElement.appendChild(link)
+}
+
+socialMedia
+  .map(createLink)
+  .map(setLinkDOM)
